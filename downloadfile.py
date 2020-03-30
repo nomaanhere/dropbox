d32e0e260aac6bbe20d1df500d12a9bb450c1b9c
Nomaan Parmar <mohamadnomaan.p@somaiya.edu>
2020-03-30 16:18:02 +0530

    Update downloadfile.py

--- downloadfile.py
+++ downloadfile.py
@@ -1,5 +1,5 @@
 import dropbox
-access_token = 'oG7g0mYYCfAAAAAAAAAACuBzidmIGZBUfy1U4tx2wSA7wW4mQt3CodpDet_nMkqL'
+access_token = '***************'
 dbx = dropbox.Dropbox(access_token)                    
 f = open("frozen_inference_graph.pb","wb")
 metadata,res = dbx.files_download("/frozen_inference_graph.pb") 
